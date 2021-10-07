a17a485ccb7edeaee09d9925e5c46c97b26a4dff
CodeLuminary <victorijoni@yahoo.com>
2021-10-07 16:59:33 +0100

    Splash screen added

--- app/src/main/java/com/sight_innovation/financemanager2/MainActivity.java
+++ app/src/main/java/com/sight_innovation/financemanager2/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){}
     }
 }
